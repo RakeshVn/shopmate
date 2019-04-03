8e238a081e253131be00fbfc056059623333067f
sys3003 <rakesh.v@way2online.co.in>
2019-04-03 18:39:44 +0530

    Radio button for sizes and colors added

--- src/components/attributesColors.jsx
+++ src/components/attributesColors.jsx
@@ -7,10 +7,29 @@ import "../assets/css/animate.css"
 
 export default class AttributesColors extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: 1
+        }
+        this.onColorChange = this.onColorChange.bind(this)
+
+    }
+    onColorChange(event) {
+        this.setState({
+            color: Number(event.target.value)
+        })
+    }
     render() {
         return (
             <div className="cs-item">
-                <input type="radio" name="cs" />
+                <input
+                    type="radio"
+                    name="color"
+                    value={Number(this.props.color.attribute_value_id)}
+                    checked={this.state.color === Number(this.props.color.attribute_value_id)}
+                    onChange={this.onColorChange} />
                 <label className={`cs-${this.props.color.attribute_value.toLowerCase()}`}></label>
             </div>
         );
--- src/components/attributesSizes.jsx
+++ src/components/attributesSizes.jsx
@@ -6,10 +6,32 @@ import "../assets/css/style.css"
 import "../assets/css/animate.css"
 
 export default class AttributesSizes extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            size: 1
+        }
+        this.onSizeChange = this.onSizeChange.bind(this)
+
+    }
+    onSizeChange(event) {
+        console.log("event", event.target.value)
+        this.setState({
+            size: Number(event.target.value)
+        })
+    }
     render() {
         return (
             <div className="sc-item">
-                <input type="radio" name={this.props.size.attribute_value_id}/>
+                <input
+                    type="radio"
+                    name="size"
+                    value={this.props.size.attribute_value_id}
+                    checked={this.state.size === Number(this.props.size.attribute_value_id)}
+                    onChange={this.onSizeChange}
+                />
                 <label>{this.props.size.attribute_value}</label>
             </div>
         );
